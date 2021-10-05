347ea62d7adc501ce37e617d64c3635e7c5ab4a5
夏先鹏 <951256916>
2021-10-05 23:02:45 +0800

    进制

---
+++ base/Hex.py
@@ -0,0 +1,17 @@
+# 十六进制
+hex_va1 = 0x13
+hex_va2 = 0Xaf
+print("hex_va1:", hex_va1)
+print("hex_va2:", hex_va2)
+
+# 八进制
+oct_val1 = 0o54
+oct_val2 = 0O17
+print("oct_val1", oct_val1)
+print("oct_val2", oct_val2)
+
+# 二进制
+bin_val1 = 0b111
+bin_val2 = 0B101
+print("bin_val1", bin_val1)
+print("bin_val2", bin_val2)
