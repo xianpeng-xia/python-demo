ddf06ee3795d4317a6b22d79dd40b59b2038e9c4
夏先鹏 <951256916>
2021-10-08 19:22:08 +0800

    字符串拼接

--- base/Str.py
+++ base/Str.py
@@ -7,3 +7,15 @@ print(str2)
 # 既包含单引号，又包含双引号，使用转义
 str3 = '"we are scared,Let\'s hide in the shade", says the bird'
 print(str3)
+
+# 字符串拼接
+# 特殊写法
+s1 = "Hello," 'Charlie'
+print(s1)
+
+s2 = "Python"
+s3 = "is Funny"
+
+# 字符串拼接
+s4 = s2 + s3
+print(s4)
