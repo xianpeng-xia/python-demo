6b0fdc1fe75cf13e924eb0391c6ff7215fe74fba
夏先鹏 <951256916>
2021-10-08 22:21:18 +0800

    赋值运算符

---
+++ base/AssignOperator.py
@@ -0,0 +1,18 @@
+# 为变量st赋值为Python
+st = "Python"
+# 为变量pi赋值为3.14
+pi = 3.14
+
+# 为变量visited赋值为True
+visited = True
+# 将变量st的值赋给st2
+st2 = st
+
+# 赋值表达式是有值的
+a = b = c = 20
+print(a)
+
+# 表达式的值可赋值给变量
+d1 = 12.34
+d2 = d1 + 5
+print(d2)
