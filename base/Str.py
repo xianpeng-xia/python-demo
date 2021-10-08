b5279f3a4b8759ae8666994c0351a9024f2ad662
夏先鹏 <951256916>
2021-10-08 19:19:40 +0800

    字符串

---
+++ base/Str.py
@@ -0,0 +1,9 @@
+# 使用不同引号
+str1 = "I'm a coder"
+# 使用转义
+str2 = 'I\'m a coder'
+print(str1)
+print(str2)
+# 既包含单引号，又包含双引号，使用转义
+str3 = '"we are scared,Let\'s hide in the shade", says the bird'
+print(str3)
