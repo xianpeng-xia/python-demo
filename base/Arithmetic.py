30d56d0d6a15f4a9fd3a8c4169415d3817dacbfa
夏先鹏 <951256916>
2021-10-08 22:36:06 +0800

    算术运算

---
+++ base/Arithmetic.py
@@ -0,0 +1,44 @@
+# 算术运算符
+
+# + 加法
+a = 5.2
+b = 3.1
+the_sum = a + b
+print("the_sun = ", the_sum)
+
+# - 减法
+the_sub = a - b
+print("the_sub = ", the_sub)
+
+# - 求负
+x = -5.0
+x = -x
+print(x)
+
+# * 乘法
+
+e = 5.2
+f = 3.1
+multiply = e * f
+print("multiply = ", multiply)
+
+# * 将n个字符串连接起来
+s3 = "s "
+print(s3 * 5)
+
+# /或// 除法
+
+# /普通除法，会产生小数
+print("19/4 = ", 19 / 4)
+print("5.2/3.1 = ", 5.2 / 3.1)
+# //整除,小数部分舍弃
+print("19/4 = ", 19 // 4)
+print("5.2/3.1 = ", 5.2 // 3.1)
+
+# % 求余
+print("5%2 = ", 5 % 2)
+print("5.2%3.1 = ", 5.2 % 3.1)
+
+# ** 乘方
+
+print("2**3 = ", 2 ** 3)
