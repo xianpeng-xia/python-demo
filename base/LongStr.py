14d48e0ce458ffeff22cf9f508dd232e4486dee6
夏先鹏 <951256916>
2021-10-08 20:07:13 +0800

    长字符串

---
+++ base/LongStr.py
@@ -0,0 +1,10 @@
+# 使用''' ''' 定义长字符串，长字符串可以包含单引号，双引号
+s = '''"Let's go fishing",said Mary.
+"OK,Let's go",said her brother.
+they walked to a lake'''
+print(s)
+
+# 可使用\对换行符进行转义，将一个字符串写成两行
+s2 = "The quick brown fox \
+jumps over the lazy dog"
+print(s2)
