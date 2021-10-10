805190881385a8786212d79d91e7cdf9007b1b73
夏先鹏 <951256916>
2021-10-10 16:55:52 +0800

    内置函数,max() 计算最大值,min() 计算最小值,len() 计算长度

---
+++ listtupledict/func.py
@@ -0,0 +1,17 @@
+# 内置函数
+
+a_tuple = (20, 1, 12, 43, 33, 222, 3)
+# max() 计算最大值
+print(max(a_tuple))
+# min() 计算最小值
+print(min(a_tuple))
+# len() 计算长度
+print(len(a_tuple))
+
+a_list = ['asd', 'fs', 'ad', 'dge']
+# max() 计算最大值
+print(max(a_list))
+# min() 计算最小值
+print(min(a_list))
+# len() 计算长度
+print(len(a_list))
